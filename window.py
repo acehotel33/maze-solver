1e6c839a3ff3942207f5cc383a60e758fc4cd78a
AceHotel33 <lomvax@gmail.com>
2024-08-28 16:21:50 +0400

    Added functionality to draw lines within Window

--- window.py
+++ window.py
@@ -3,8 +3,13 @@ import time
 
 def main():
     print("running...")
-    testWindow = Window(200, 200)
-    testWindow.wait_for_close()
+    window = Window(200, 200)
+    point_a = Point(15, 15)
+    point_b = Point(185, 185)
+    line_a_b = Line(point_a, point_b)
+    fill_color = "red"
+    window.draw_line(line_a_b, fill_color)
+    window.wait_for_close()
     
 
 class Window:
@@ -31,6 +36,28 @@ class Window:
     def close(self):
         self.__window_running = False
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point_a, point_b):
+        self.point_a = point_a
+        self.point_b = point_b
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.point_a.x,
+            self.point_a.y,
+            self.point_b.x,
+            self.point_b.y,
+            fill=fill_color,
+            width=2
+        )
 
 main()
 
